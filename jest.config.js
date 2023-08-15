2a98f9448f55b3d7e3e7d8bdcf0b0c4d71ac588e
Harang <dbd02169@naver.com>
2023-08-15 15:57:39 +0900

    Revert "Revert "chore: 초기 세팅 및 dependency 업데이트""

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,6 @@ const customJestConfig = {
     '<rootDir>/jest.setup.js',
   ],
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
