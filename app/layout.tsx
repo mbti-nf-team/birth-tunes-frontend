f0668742c5101f6b60abe718572bedd46be9b1cc
saseungmin <dbd02169@naver.com>
2023-08-26 02:51:28 +0900

    perf(next/font): next/font/local을 적용하여 폰트를 불러오도록 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import localFont from 'next/font/local';
+
 import Layout from '../components/Layout';
 
 import Providers from './providers';
@@ -19,11 +21,38 @@ export const metadata = {
   },
 };
 
+const dungGeunMoFont = localFont({
+  src: [
+    {
+      path: './fonts/korean/DungGeunMo.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  fallback: [
+    'DungGeunMo',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
+
 function RootLayout({ children }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={dungGeunMoFont.className}>
       <head>
         <script
           async
