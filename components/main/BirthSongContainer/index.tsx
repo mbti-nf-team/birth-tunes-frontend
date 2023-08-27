dd3a6e70dfd11e3eedd44270ae5a4131b3d7a0aa
saseungmin <dbd02169@naver.com>
2023-08-27 23:51:23 +0900

    feat: 결과 링크 공유하기 구현

--- components/main/BirthSongContainer/index.tsx
+++ components/main/BirthSongContainer/index.tsx
@@ -1,16 +1,27 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { useSearchParams } from 'next/navigation';
 
 import BirthSongForm from '../BirthSongForm';
 import BirthSongResult from '../BirthSongResult';
 
 function BirthSongContainer() {
+  const { get } = useSearchParams();
   const [birthDate, setBirthDate] = useState<string>('');
+
+  useEffect(() => {
+    const defaultBirthDate = get('date');
+
+    if (defaultBirthDate) {
+      setBirthDate(defaultBirthDate);
+    }
+  }, []);
 
   return (
     <>
-      <BirthSongForm onSubmit={setBirthDate} />
+      <BirthSongForm defaultBirthDate={birthDate} onSubmit={setBirthDate} />
       <BirthSongResult birthDate={birthDate} />
     </>
   );
--- components/main/BirthSongResult/index.tsx
+++ components/main/BirthSongResult/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 
 import { fetchMusicChartSong } from '../../../lib/apis/search';
+import Button from '../../common/Button';
 import IframeVideoPlayer from '../IframeVideoPlayer';
 
 import styles from './index.module.scss';
@@ -22,24 +23,36 @@ function BirthSongResult({ birthDate }: Props) {
     staleTime: Infinity,
   });
 
+  const onClickShareLink = async () => {
+    try {
+      await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_ORIGIN}?date=${birthDate}`);
+    } catch (error) {
+      // TODO - 실패 케이스 정의 후 변경
+      console.error(error);
+    }
+  };
+
   if (!isSuccess) {
     return null;
   }
 
   return (
-    <div className={styles.resultWrapper}>
-      <div className={styles.resultContentsWrapper}>
-        <div className={styles.titleWrapper}>
-          <div>
-            {findBirthSong.artist}
-          </div>
-          <div>
-            {findBirthSong.title}
+    <>
+      <div className={styles.resultWrapper}>
+        <div className={styles.resultContentsWrapper}>
+          <div className={styles.titleWrapper}>
+            <div>
+              {findBirthSong.artist}
+            </div>
+            <div>
+              {findBirthSong.title}
+            </div>
           </div>
         </div>
+        <IframeVideoPlayer youtubeVideoId={findBirthSong.youtube_video_id} />
       </div>
-      <IframeVideoPlayer youtubeVideoId={findBirthSong.youtube_video_id} />
-    </div>
+      <Button buttonType="secondary" type="button" onClick={onClickShareLink}>결과 공유하기</Button>
+    </>
   );
 }
 
