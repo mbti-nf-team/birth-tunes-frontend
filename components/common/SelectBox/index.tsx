3c5c7c50313c4b6f823bb386087ad3f63ec928fc
saseungmin <dbd02169@naver.com>
2023-08-29 04:22:13 +0900

    style: select icon 추가 및 width값 수정

--- components/common/SelectBox/index.tsx
+++ components/common/SelectBox/index.tsx
@@ -1,6 +1,10 @@
 import { DetailedHTMLProps, PropsWithChildren, SelectHTMLAttributes } from 'react';
 
+import Image from 'next/image';
+
 import clsx from 'clsx';
+
+import chevronIcon from '../../../lib/assets/chevron.svg';
 
 import styles from './index.module.scss';
 
@@ -18,20 +22,25 @@ function SelectBox({
   id, selectType = 'secondary', children, emptyOption, className, ...rest
 }: PropsWithChildren<Props>) {
   return (
-    <select
-      id={id}
-      name={id}
-      className={clsx(styles.selectWrapper, {
-        [styles[selectType]]: selectType,
-      }, className)}
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...rest}
-    >
-      {emptyOption && (
-        <option value="" disabled>{emptyOption}</option>
-      )}
-      {children}
-    </select>
+    <div className={styles.selectBoxWrapper}>
+      <select
+        id={id}
+        name={id}
+        className={clsx(styles.selectWrapper, {
+          [styles[selectType]]: selectType,
+        }, className)}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...rest}
+      >
+        {emptyOption && (
+          <option value="" disabled>{emptyOption}</option>
+        )}
+        {children}
+      </select>
+      <span className={styles.iconWrapper}>
+        <Image className={styles.icon} src={chevronIcon} alt="icon" />
+      </span>
+    </div>
   );
 }
 
