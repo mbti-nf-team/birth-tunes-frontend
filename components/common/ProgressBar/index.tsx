00db578b60d1a3db572404412b728dd54eba2653
saseungmin <dbd02169@naver.com>
2023-09-01 23:58:48 +0900

    feat: 로딩을 위한 progress bar 컴포넌트 구현

---
+++ components/common/ProgressBar/index.tsx
@@ -0,0 +1,60 @@
+import { useNProgress } from '@tanem/react-nprogress';
+import { motion, Variants } from 'framer-motion';
+
+import styles from './index.module.scss';
+
+const progressVariants: Variants = {
+  none: {
+    opacity: 0,
+    transitionEnd: {
+      visibility: 'hidden',
+      display: 'none',
+    },
+  },
+  visible: {
+    opacity: 1,
+    visibility: 'visible',
+    display: 'flex',
+  },
+};
+
+interface Props {
+  isAnimating: boolean;
+  animationDuration?: number;
+  incrementDuration?: number;
+  minimum?: number;
+}
+
+function ProgressBar({ isAnimating, ...rest }: Props) {
+  const { animationDuration, isFinished, progress } = useNProgress({
+    isAnimating,
+    ...rest,
+  });
+
+  return (
+    <motion.div
+      className={styles.progressContainer}
+      initial="none"
+      animate={isFinished ? 'none' : 'visible'}
+      variants={progressVariants}
+    >
+      <div className={styles.progressText}>
+        <div>{`${Math.floor(progress * 100)}%`}</div>
+        <div>Loading...</div>
+      </div>
+      <div
+        className={styles.progressBarWrapper}
+      >
+        <div
+          className={styles.progressBar}
+          style={{
+            transition: `width ${animationDuration}ms linear`,
+            width: `${(progress * 100)}%`,
+          }}
+        />
+      </div>
+    </motion.div>
+  );
+}
+
+export default ProgressBar;
